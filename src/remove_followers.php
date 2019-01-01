285f49ab01235d45549ec10a0fd2bb4bb16ba8f7
Yzy <yangzhyo@gmail.com>
2019-01-01 20:22:05 +0800

    bug fix

--- src/remove_followers.php
+++ src/remove_followers.php
@@ -2,7 +2,7 @@
 require __DIR__.'/../vendor/autoload.php';
 $ig = new \InstagramAPI\Instagram();
 $ig->setProxy('socks5://127.0.0.1:1086');
-$ig->login('yangzhyo','testtest');
+$ig->login('test','test');
 $followers = $ig->people->getSelfFollowers('123e4567-e89b-12d3-a456-426655440000');
 print_r('Totally '.count($followers->getUsers())." followers\n");
 foreach ($followers->getUsers() as $user) {
--- src/remove_followings.php
+++ src/remove_followings.php
@@ -2,7 +2,7 @@
 require __DIR__.'/../vendor/autoload.php';
 $ig = new \InstagramAPI\Instagram();
 $ig->setProxy('socks5://127.0.0.1:1086');
-$ig->login('yangzhyo','testtest');
+$ig->login('test','test');
 $followings = $ig->people->getSelfFollowing('123e4567-e89b-12d3-a456-426655440000');
 print_r('Totally '.count($followers->getUsers())." followings\n");
 foreach ($followings->getUsers() as $user) {
