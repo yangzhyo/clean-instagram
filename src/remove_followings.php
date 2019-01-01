8fa6b4d9f6c45b14522ec2760fa4e3f26f7f0cd0
Yzy <yangzhyo@gmail.com>
2019-01-01 20:11:09 +0800

    fix a variable name

--- src/remove_followings.php
+++ src/remove_followings.php
@@ -3,9 +3,9 @@ require __DIR__.'/../vendor/autoload.php';
 $ig = new \InstagramAPI\Instagram();
 $ig->setProxy('socks5://127.0.0.1:1086');
 $ig->login('yangzhyo','testtest');
-$followers = $ig->people->getSelfFollowing('123e4567-e89b-12d3-a456-426655440000');
+$followings = $ig->people->getSelfFollowing('123e4567-e89b-12d3-a456-426655440000');
 print_r('Totally '.count($followers->getUsers())." followings\n");
-foreach ($followers->getUsers() as $user) {
+foreach ($followings->getUsers() as $user) {
     print_r('Remove '.$user->getPk()."\n");
     $ig->people->unfollow($user->getPk());
 }
