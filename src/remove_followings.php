534fb8deed0fa4c8c984a882febef67e615d51f8
Yzy <yangzhyo@gmail.com>
2019-01-01 20:38:27 +0800

    bug fix

--- src/remove_followings.php
+++ src/remove_followings.php
@@ -4,7 +4,7 @@ $ig = new \InstagramAPI\Instagram();
 $ig->setProxy('socks5://127.0.0.1:1086');
 $ig->login('test','test');
 $followings = $ig->people->getSelfFollowing('123e4567-e89b-12d3-a456-426655440000');
-print_r('Totally '.count($followers->getUsers())." followings\n");
+print_r('Totally '.count($followings->getUsers())." followings\n");
 foreach ($followings->getUsers() as $user) {
     print_r('Remove '.$user->getPk()."\n");
     $ig->people->unfollow($user->getPk());
